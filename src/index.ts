7579c2472b592e41d34ad537f0ee43c3eb78d4b1
Fabrício <fabricio_c_anelli@hotmail.com>
2022-11-17 22:42:49 -0300

    #3 - Add cors and configure

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { router } from "./router";
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
-app.use(express.json());
-app.use(router);
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(router);
