9a72d60c0806a78e66a85591a468a6962f176061
Fabrício <fabricio_c_anelli@hotmail.com>
2022-11-18 23:55:33 -0300

    #5 - Incorporating the tournament response

--- src/controllers/Tournaments/listTournaments.ts
+++ src/controllers/Tournaments/listTournaments.ts
@@ -3,7 +3,7 @@ import { db } from "../../databases/db";
 
 const listTournaments = async (req: Request, res: Response) => {
   const SQL =
-    "SELECT tournament_name from tournaments ORDER BY tournament_name ASC;";
+    "SELECT t.id, t.tournament_name, c.country_name, cont.continent_name from tournaments AS t INNER JOIN countrys AS c ON c.id = t.tournament_region INNER join continents AS cont ON c.continent_id = cont.id ORDER BY t.tournament_name ASC;";
 
   try {
     const response: any = await db.promise().query(SQL);
